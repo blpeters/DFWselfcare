b14983029846061ef71817f0b25be21447fd6f36
Brett <48160329+blpeters@users.noreply.github.com>
2024-10-01 19:04:55 -0500

    Finished R script - all available ACS years

--- DFWselfcare.R
+++ DFWselfcare.R
@@ -1,11 +1,8 @@
 library(tidyverse)
 library(tidycensus)
 library(ggplot2)
-library(Hmisc)
 library(sf)
-library(leaflet)
 library(stringr)
-library(htmlwidgets)
 library(dplyr)
 
 counties <- c(085, 'Dallas', 'Denton', 'Ellis', 'Hunt', 'Kaufman', 'Rockwall',
@@ -21,31 +18,59 @@ Variables<-c('B18106_004', # *male self care disabilities by age
              'B18106_029', # *
              'B18106_032'  # *
              )
-selfcare2022 <- get_acs(geography = "tract",
-                        state = 'TX',
-                        county = counties,
-                        variables = Variables, 
-                        survey = "acs5",     
-                        year = 2022,   
-                        geometry=T
-                        )
-totalpop2022 <-get_acs(geography = "tract", 
-                   state = 'TX',
-                   county = counties,
-                   variables = 'B01003_001', 
-                   survey = "acs5",     
-                   year = 2022, 
-                   ) 
+
+get_selfcare_data <- function(year) {
+  selfcare <- self_care(year)
+}
+
+get_total_pop <- function(year) {
+  totalpop <- total_pop(year)
+}
+
+self_care <- function(year) {
+return(get_acs(geography = "tract",
+                state = 'TX',
+                county = counties,
+                variables = Variables, 
+                survey = "acs5",     
+                year = year,   
+                geometry=T
+                ))
+}
+
+total_pop <- function(year) {
+return(get_acs(geography = "tract", 
+                state = 'TX',
+                county = counties,
+                variables = 'B01003_001', 
+                survey = "acs5",     
+                year = year,
+                )) 
+}
+
 # Group individual age groups into a total population with self-care disabilities
-total2022 <- selfcare2022 %>% 
+combine_ages <- function(selfcaredata) {
+  total <- selfcaredata %>% 
   group_by(NAME) %>% 
   summarise(self_care_pop = sum(estimate))
-# Merge total population to the self care population sf object and calculate percent
-df2022 <- merge(total2022, totalpop2022)
-df2022$selfcare_percent <- df2022$self_care_pop / df2022$estimate * 100
-# plot map
-ggplot(df2022) + 
+  return (total) 
+}
+
+plot_selfcare <- function(df) {
+  ggplot(df) + 
   geom_sf(aes(fill = selfcare_percent)) +
   scale_fill_viridis_c(option = 'inferno') +
-  labs(title = '2022', fill = "% of Pop. with a Self-care Disability") +
+  labs(fill = "% of Pop. with a Self-care Disability") +
   theme_void()
+}
+
+
+# Run Script:
+
+selfcare <- get_selfcare_data(2021)
+totalpop <- get_total_pop(2021) 
+total <- combine_ages(selfcare)
+df <- merge(total, totalpop)
+df$selfcare_percent <- df$self_care_pop / df$estimate * 100
+# plot_selfcare(df)
+st_write(df, "output/selfcare2021.shp", append = FALSE)
