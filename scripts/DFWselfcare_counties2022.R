fa62e76c2bf44bdb9b9cb9215b056aad19f50293
Brett <48160329+blpeters@users.noreply.github.com>
2024-10-03 12:06:13 -0500

    lollipop bar chart and map

--- scripts/DFWselfcare_counties2022.R
+++ scripts/DFWselfcare_counties2022.R
@@ -28,7 +28,7 @@ get_total_pop <- function(year) {
 }
 
 self_care <- function(year) {
-return(get_acs(geography = "tract",
+return(get_acs(geography = "county",
                 state = 'TX',
                 county = counties,
                 variables = Variables, 
@@ -39,7 +39,7 @@ return(get_acs(geography = "tract",
 }
 
 total_pop <- function(year) {
-return(get_acs(geography = "tract", 
+return(get_acs(geography = "county", 
                 state = 'TX',
                 county = counties,
                 variables = 'B01003_001', 
@@ -59,11 +59,33 @@ combine_ages <- function(selfcaredata) {
 plot_selfcare <- function(df) {
   ggplot(df) + 
   geom_sf(aes(fill = selfcare_percent)) +
-  scale_fill_viridis_c(option = 'inferno') +
-  labs(fill = "% of Pop. with a Self-care Disability") +
+  scale_fill_viridis_c(option = 'viridis') +
+  labs(main = 'DFW Self-Care Disabilities 2022', fill = "% of Pop. with a Self-care Disability") +
   theme_void()
 }
 
+lollipop <- function(df) {
+  ggplot(df, aes(x = reorder(county, selfcare_percent), y = selfcare_percent)) +
+    geom_segment(aes(x = reorder(county, selfcare_percent), xend = reorder(county, selfcare_percent), 
+                     y = 0, yend = selfcare_percent),
+                 color = 'black', lwd = 1) +
+    geom_point(size = 4, pch = 21, bg = 4, col = 1) +
+    coord_flip() +
+    theme(  # Adjust axis, labels, grid, etc.
+      legend.position = "none",
+      axis.ticks.length = unit(0, "mm"),
+      axis.text.y = element_text(hjust = 1.5),
+      panel.grid = element_blank()
+    ) +
+    labs(title = "% Population with Self-Care Disability", 
+         subtitle = "Dallas-Fort Worth Counties",
+         x = "County",
+         y = "% of Population with Self-Care Disability")  +
+    theme_bw()
+}
+  
+# Separate county text from texas Text:
+df$county <- sub(" .*", "", df$NAME)
 
 # Run Script:
 
@@ -73,4 +95,11 @@ total <- combine_ages(selfcare)
 df <- merge(total, totalpop)
 df$selfcare_percent <- df$self_care_pop / df$estimate * 100
 plot_selfcare(df)
-st_write(df, "output/selfcare2021.shp", append = FALSE)
+lollipop(df)
+
+
+ggplot(df) +
+  geom_sf(aes(fill = county), fill = 'white', alpha = 0) +
+  geom_sf_label(aes(label = county)) +
+  theme_void()
+
